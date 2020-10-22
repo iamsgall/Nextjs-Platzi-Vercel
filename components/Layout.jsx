ddfa84aa92069c244cd1ee9af44c8d06c538088e
ico4e <70281513+ico4e@users.noreply.github.com>
2020-10-22 12:58:22 -0400

    Nextjs Platzi Lesson 19-21

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,24 @@
 import styles from '../styles/Layout.module.sass';
 import Link from 'next/link';
 import Head from 'next/head';
+import NProgress from 'nprogress';
+import {useRouter} from 'next/router';
+import {useEffect} from 'react';
 
 export default function Layout({title, children}) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', () => NProgress.start());
+    router.events.on('routeChangeComplete', () => NProgress.done());
+    router.events.on('routeChangeError', () => NProgress.done());
+    return () => {
+      router.events.off('routeChangeStart', () => NProgress.start());
+      router.events.off('routeChangeComplete', () => NProgress.done());
+      router.events.off('routeChangeError', () => NProgress.done());
+    };
+  }, []);
+
   return (
     <div>
       <Head>
